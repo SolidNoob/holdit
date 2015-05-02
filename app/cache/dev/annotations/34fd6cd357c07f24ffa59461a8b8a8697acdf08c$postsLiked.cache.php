765c13add2d634161b65b18b682389c871852f05
noobPHP <anthony.cammarata@hotmail.com>
2015-05-02 11:28:52 +0200

    Deleting useless files

--- app/cache/dev/annotations/34fd6cd357c07f24ffa59461a8b8a8697acdf08c$postsLiked.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:31:"Doctrine\\ORM\\Mapping\\ManyToMany":7:{s:12:"targetEntity";s:27:"Noob\\PostBundle\\Entity\\Post";s:8:"mappedBy";s:6:"likers";s:10:"inversedBy";N;s:7:"cascade";N;s:5:"fetch";s:10:"EXTRA_LAZY";s:13:"orphanRemoval";b:0;s:7:"indexBy";N;}}');
--- app/cache/dev/annotations/34fd6cd357c07f24ffa59461a8b8a8697acdf08c$usersFollowers.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:31:"Doctrine\\ORM\\Mapping\\ManyToMany":7:{s:12:"targetEntity";s:27:"Noob\\UserBundle\\Entity\\User";s:8:"mappedBy";s:12:"usersIFollow";s:10:"inversedBy";N;s:7:"cascade";N;s:5:"fetch";s:10:"EXTRA_LAZY";s:13:"orphanRemoval";b:0;s:7:"indexBy";N;}}');
